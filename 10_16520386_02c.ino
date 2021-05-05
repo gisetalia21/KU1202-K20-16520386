6ae78c0ddd15af0f1d5c5eef4a2594007817a3bc
gisetalia21 <76264296+gisetalia21@users.noreply.github.com>
2021-05-06 04:08:31 +0700

    kode minggu 10 exercise 01c

---
+++ 10_16520386_02c.ino
@@ -0,0 +1,19 @@
+void setup()
+{
+  pinMode(0, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(0, HIGH);
+  digitalWrite(10, HIGH);
+  analogWrite(11, 0);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
